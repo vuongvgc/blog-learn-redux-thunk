2fa63919a8214e957e22fbb02e835de121771964
vuongvgc <vuongvgc@gmail.com>
2020-11-17 21:49:51 +0700

    build component Post List

---
+++ src/Components/PostList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class PostList extends React.Component {
+    render(){
+        return(
+            <div>PostList</div>
+        )
+    }
+}
+export default PostList;
