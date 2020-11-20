d94ca400191b12ac23c33cbb6ca4b85863e45faf
vuongvgc <vuongvgc@gmail.com>
2020-11-20 09:20:14 +0700

    use memozie to imporve perfomance: other issue

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -13,9 +13,9 @@ export const fetchPost = () => {
 // }
 // eslint-disable-next-line no-undef
 var _ = require('lodash')
-export const fetchUser = _.memoize(function(id){
-    return  async function(dispatch){
+export const fetchUser = function(id){
+    return  _.memoize(async function(dispatch){
         const response = await jsonPlaceholder.get(`/users/${id}`);
         dispatch({type: 'FETCH__USER', payload: response.data});
-    }
-});
+    });
+};
